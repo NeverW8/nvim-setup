def74c0dc8795e9528414ac5b4549e5cd4f9fd74
NeverW8 <carlerikskantz@gmail.com>
2023-03-12 18:03:16 +0100

    adding my local config for nvim

---
+++ lua/fiskenhero/remap.lua
@@ -0,0 +1,31 @@
+
+vim.g.mapleader = ","
+vim.keymap.set("n", "<leader>o", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+
+-- Nerdtree
+vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeToggle)
+
+-- greatest remap ever
+--vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- hax
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Telescope FF
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+--asd
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
