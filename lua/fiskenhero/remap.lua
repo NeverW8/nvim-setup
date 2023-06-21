4c1a0667e6995fbd18c831f43084b4e7b90318b0
NeverW8 <carlerikskantz@gmail.com>
2023-06-21 14:52:42 +0200

    added new.tab shotcut to leader+t

--- lua/fiskenhero/remap.lua
+++ lua/fiskenhero/remap.lua
@@ -33,4 +33,5 @@ end)
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
-
+-- new tab with leader + t
+vim.keymap.set("n", "<leader>t", vim.cmd.tabnew)
