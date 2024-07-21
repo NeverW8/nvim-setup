b7c6dea63731b78b26125f9223dfb77498422f27
NeverW8 <carl.skantz@carlskantz.se>
2024-07-21 23:23:51 +0200

    Pretty colours

--- lua/fiskenhero/lazy/colors.lua
+++ lua/fiskenhero/lazy/colors.lua
@@ -2,8 +2,8 @@ function ColorMyPencils(color)
     color = color or "rose-pine-main"
     vim.cmd.colorscheme(color)
 
---    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "Normal", { bg = "#332138" })
+--    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#332138" })
 end
 
 return {
@@ -64,10 +64,70 @@ return {
         name = "rose-pine",
         config = function()
             require('rose-pine').setup({
-                disable_background = false,
-                styles = {
-                    italic = false,
+                variant = "auto",
+                dark_variant = "main",
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true,
+                    migrations = true,
                 },
+
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
+                },
+
+                groups = {
+                    border = "muted",
+                    link = "iris",
+                    panel = "surface",
+
+                    error = "love",
+                    hint = "iris",
+                    info = "foam",
+                    note = "pine",
+                    todo = "rose",
+                    warn = "gold",
+
+                    git_add = "foam",
+                    git_change = "rose",
+                    git_delete = "love",
+                    git_dirty = "rose",
+                    git_ignore = "muted",
+                    git_merge = "iris",
+                    git_rename = "pine",
+                    git_stage = "iris",
+                    git_text = "rose",
+                    git_untracked = "subtle",
+
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                },
+
+                before_highlight = function(group, highlight, palette)
+                    -- Disable all undercurls
+                    -- if highlight.undercurl then
+                    --     highlight.undercurl = false
+                    -- end
+                    --
+                    -- Change palette colour
+                    -- if highlight.fg == palette.pine then
+                    --     highlight.fg = palette.foam
+                    -- end
+                end,
             })
             ColorMyPencils("rose-pine-main")
         end
